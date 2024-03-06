10ad9ec630352f1907e56ff23c72c4ab69cb47a2
02_bao <104081842+02bao@users.noreply.github.com>
2024-03-06 13:17:21 +0700

    update

---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
+    }
+}
